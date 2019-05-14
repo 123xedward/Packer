555775f5daa227955e75e7431d774dc3b5347f7f
Edward Montes Hernández <edward.montesh@gmail.com>
2019-05-14 10:26:45 -0500

    Packer

--- medatataAWS/provision.sh
+++ medatataAWS/provision.sh
@@ -12,3 +12,17 @@ echo "installing ansible"
 
 ansible --version
 ls -l
+
+# Script metadata
+sudo yum -y install httpd
+sudo chkconfig --levels 235 httpd on
+sudo service httpd start
+sudo yum -y install php
+sudo service httpd restart
+sudo mkdir /var/www/html/css
+
+#Agente SSM
+sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+sudo systemctl status amazon-ssm-agent
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
